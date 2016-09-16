ee548d1006d44c00455648251fcb45b7906cc296
yuko <moneco.kiriya@gmail.com>
2016-09-16 18:41:28 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 管理画面動作環境
+
+## Ruby version
+  2.3.1
+## Rails version
+  4.2.7
---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,14 @@
+class TweetsController < ApplicationController
+  require "twitter"
+
+  def tweet
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.consumer_key
+      config.consumer_secret     = Rails.application.secrets.consumer_secret
+      config.access_token        = Rails.application.secrets.access_token
+      config.access_token_secret = Rails.application.secrets.access_token_secret
+    end
+
+    client.update("テスト")
+  end
+end
